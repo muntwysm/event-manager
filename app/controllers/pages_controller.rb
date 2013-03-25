d65f2ec4c6c37d0d2254773089f0af99606205bd
muntwysm <stefan.muntwyler@gmail.com>
2013-03-25 16:50:20 +0100

    adds blueprint stylesheet

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+	#before_filter :require_admin, :only => :admin
+
+  def home
+		@title = "Home"
+		#@events = Event.all(:conditions => ["date > ?", Date.today])
+		#@contributions = Contribution.paginate :page => params[:page], :per_page => 20
+  end
+
+  def help
+		@title = "Help"
+  end
+
+  def about
+		@title = "About"
+  end
+
+  def admin
+		@title = "Admin"
+  end
+
+end
